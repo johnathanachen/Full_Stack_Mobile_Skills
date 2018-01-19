c883b2f26db8387fe3d1bdf5e614eb0c8404ea4f
Johnathan Chen <johnathanchenn@outlook.com>
2018-01-18 18:00:37 -0800

    table view challenge completed

--- Table View/Demo/PrettyIcons/PrettyIcons/PrettyIconsTableViewController.swift
+++ Table View/Demo/PrettyIcons/PrettyIcons/PrettyIconsTableViewController.swift
@@ -9,43 +9,46 @@
 import UIKit
 
 class PrettyIconsTableViewController: UITableViewController {
+    
+    var icons = [Icon]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        let iconSets = IconSet.iconSets()
+        let iconSet = iconSets[0]
+        icons = iconSet.icons
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return icons.count
     }
 
-    /*
+  
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
 
-        // Configure the cell...
+        let icon = icons[indexPath.row]
+        cell.textLabel?.text = icon.title
+        cell.detailTextLabel?.text = icon.subtitle
+        if let iconImage = icon.image {
+            cell.imageView?.image = iconImage
+        }
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
