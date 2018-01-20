84e5f5dfaba74fa263bcf0812511aa5639ee2d59
Johnathan Chen <johnathanchenn@outlook.com>
2018-01-19 22:06:36 -0800

    frameworks

---
+++ Xcode/Frameworks/Checkbox/Checkbox/Checkbox.h
@@ -0,0 +1,19 @@
+//
+//  Checkbox.h
+//  Checkbox
+//
+//  Created by Johnathan Chen on 1/19/18.
+//  Copyright © 2018 JCSwifty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Checkbox.
+FOUNDATION_EXPORT double CheckboxVersionNumber;
+
+//! Project version string for Checkbox.
+FOUNDATION_EXPORT const unsigned char CheckboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Checkbox/PublicHeader.h>
+
+
